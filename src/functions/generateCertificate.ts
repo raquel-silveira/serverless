d83bdaaa60b4b29f45937161db967ad3cbf85f02
strixsoul <rsilveira136@gmail.com>
2021-08-28 14:39:29 -0300

    generating certificate.pdf with template

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,30 +1,94 @@
+import chromium from "chrome-aws-lambda";
+import path from "path";
+import handlebars from "handlebars";
+import fs from "fs";
+import dayjs from "dayjs";
+import { S3 } from "aws-sdk";
+
 import { document } from "../utils/dynamodbClient";
 
 interface ICreateCertificate {
-    id: string;
-    name: string;
-    grade: string;
+  id: string;
+  name: string;
+  grade: string;
 }
 
+interface ITemplate {
+  id: string;
+  name: string;
+  grade: string;
+  date: string;
+  medal: string;
+}
+
+const compile = async function (data: ITemplate) {
+  const filePath = path.join(
+    process.cwd(),
+    "src",
+    "templates",
+    "certificate.hbs"
+  );
+
+  const html = fs.readFileSync(filePath, "utf-8");
+
+  return handlebars.compile(html)(data);
+};
+
 export const handle = async (event) => {
-    const { id, name, grade} = JSON.parse(event.body) as ICreateCertificate;
+  const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
 
-    await document.put({
-        TableName: "users_certificates",
-        Item: {
-            id,
-            name,
-            grade
-        }
-    }).promise();
+  await document
+    .put({
+      TableName: "users_certificates",
+      Item: {
+        id,
+        name,
+        grade,
+      },
+    })
+    .promise();
 
-    return {
-        statusCode: 201,
-        body: JSON.stringify({
-            message: "Certificate created!"
-        }),
-        headers: {
-            "Content-Type": "application/json",
-        }
+  const medalPath = path.join(process.cwd(), "src", "templates", "selo.png");
+  const medal = fs.readFileSync(medalPath, "base64");
+
+  const data: ITemplate = {
+    date: dayjs().format("DD/MM/YYYY"),
+    grade,
+    name,
+    id,
+    medal,
+  };
+
+  const content = await compile(data);
+
+  const browser = await chromium.puppeteer.launch({
+    headless: true,
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+  });
+
+  const page = await browser.newPage();
+
+  await page.setContent(content);
+
+  const pdf = await page.pdf({
+    format: "a4",
+    landscape: true,
+    // path: process.env.IS_OFFLINE ? "certificate.pdf" : null,
+    printBackground: true,
+    preferCSSPageSize: true,
+  });
+
+  await browser.close();
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify({
+        message: "Certificate created!"
+    }),
+    headers: {
+        "Content-Type": "application/json",
     }
+  }
 }
